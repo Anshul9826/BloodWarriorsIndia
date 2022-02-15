ff701c4130c886eb5d23a2d883f81b35727dde2d
Ayushshinde88 <ayushshinde032@gmail.com>
2022-02-15 17:39:03 +0530

    changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,14 +3,11 @@ import "./About.css";
 
 function About() {
   return (
-    <div>About page bhi rahul dega.</div>
+    <div>About page bhi rahul dega.
+      amnkankankvknnv
+    </div>
   )
 }
 
-<<<<<<< HEAD
 export default About;
 
-Surprise Surprise Motherfucker :)
-=======
-export default About
->>>>>>> 77c8d4db46e76c1b82984f32e0ee81e55d8fe085
