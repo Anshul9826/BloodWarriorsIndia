1b7aa2fc15c28a4857dfbeab9543e25562d2f57b
rahul <rahulprajapat41437@gmail.com>
2022-02-24 19:26:28 +0530

    about final

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,6 @@ import "./About.css";
 function About() {
   return (
     <>
-<<<<<<< HEAD
       <div className="main d-flex flex-column">
         <div className=" d-flex align-items-center my-3 m-auto">
           <h1>
@@ -47,22 +46,6 @@ function About() {
             We saw and felt that how a person seek and struggle for blood when
             someone of his own going through that disease so we started a (BWI)
             platform where we reduce trouble of someone and are trying to find
-=======
-      <div className="main">
-        <div className="aboutHeader">
-          {/*<img src="/image/upper.jpg" alt=""/> */}
-          <h1 className="start">
-            About <span id="red">Blood</span> Warriors{" "}
-            <img src="/image/heart_anime.gif" alt="heart" />{" "}
-          </h1>
-        </div>
-        <div className="tank">
-          <div className="wer">How WE Are Unique?</div>
-          <div className="exp">
-            we saw and felt that how a person seek and struggle for blood when
-            someone of his own going through that disease so we started a (BWI)
-            platform where we reduce trouble of someone and are tryinnh to find
->>>>>>> 7bbe315cbe811132c998c80e493c87d520cdfccc
             blood easily. Apart from that someone can start a new life when
             he/she gets blood through us. A platform where we provide blood
             seeker and where people connect to each other by which people aware
@@ -70,7 +53,6 @@ function About() {
             humanity and all we are doing related to blood because our main
             focus to donater who donate the blood by helping a little from our
             side.{" "}
-<<<<<<< HEAD
           </h5>
         </div>
         <div className="section-2 d-flex m-4">
@@ -79,51 +61,21 @@ function About() {
               Our Mission <i className="fa-solid fa-hand-holding-droplet"></i>
             </h1>
             <h5>
-=======
-          </div>
-        </div>
-        <div className="container">
-          <div className="para2">
-            <h1 id="design">
-              Our Mission <i className="fa-solid fa-hand-holding-droplet"></i>
-            </h1>
-
-            <p>
->>>>>>> 7bbe315cbe811132c998c80e493c87d520cdfccc
               Blood warrior is an organization which is trying to help both
               blood donor and blood requestor by using blockchain technology.
               What we are tryinng to do is whenever a person donates blood he
               gets a token from the hospital/Blood donation center. These token
               can be redeemed by the donor at any local buisness(or sponsor) in
-<<<<<<< HEAD
               exchange of discount and promotions.
             </h5>
             <p>
               In this way we are creating a virtual circle of value exchange
               between three parties{" "}
-=======
-              exchange of discount/Promotions.
-              <br />
-            </p>
-            <div className="connect">
-              <img
-                src="https://media.giphy.com/media/xUA7biF7n8LcAqh4bK/giphy.gif"
-                alt=""
-              />
-            </div>
-            <div className="par">
-              <strong>
-                In this way we are creating a virtual circle of value exchange
-                between three parties{" "}
-              </strong>
-
->>>>>>> 7bbe315cbe811132c998c80e493c87d520cdfccc
               <ol type="1">
                 <li id="id">eligible life donors</li>
                 <li id="id">Donation centres</li>
                 <li id="id">Local buisness/Sponsors</li>
               </ol>
-<<<<<<< HEAD
             </p>
           </div>
         </div>
@@ -142,88 +94,6 @@ function About() {
           </div>
         </div>
       </div>
-=======
-            </div>
-          </div>
-
-          <div id="parImg">
-            <img
-              className="para3"
-              src="/image/image1.webp"
-              alt="about mission"
-            />
-          </div>
-        </div>
-        <div className="container1">
-          <div className="container3">
-            <div className="para6">
-              <h1 id="design">
-                Our Vision <i className="fa-solid fa-glasses"></i>{" "}
-              </h1>
-
-              <p>
-                At least every healthy person should donate blood 2 time a year.
-                and noone should die due to lack of blood and donator should
-                also get something in exchange of blood. And we are also trying
-                to promote local buisness which will help in increasing GDP of
-                india.
-              </p>
-            </div>
-          </div>
-          <div className="block">
-            <div className="same" id="one"></div>
-            <div className="same" id="two"></div>
-            <div className="same" id="three"></div>
-            <div className="same" id="four"></div>
-          </div>
-          <div className="container2">
-            <div className="thinking">
-              <img src="/image/gif/day1.gif" alt=" null" />
-            </div>
-          </div>
-        </div>
-        <div className="container4">
-          <div className="tank1">
-            <h1 id="gifHeart">
-              {" "}
-              Message From Our Side{" "}
-              <img
-                src="https://media.giphy.com/media/39QWHcIa5L9q37sorp/giphy.gif"
-                alt=" null"
-              />
-            </h1>
-            <p>
-              We know that there is only one God even though we call all doctors
-              the real life god.But everyone can't be the doctors instead they
-              can be the part of the god by doanating blood and save other's
-              lives.
-            </p>
-            <p>
-              We blood warriors India requesting you all atleast donate your
-              blood two times in a year.Your one donation can save life upto
-              three people.{" "}
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="tank2">
-        <img
-          src="https://media.giphy.com/media/xwNyjvAKjWOch1H1wn/giphy.gif"
-          alt=""
-        />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
-        perferendis, laboriosam recusandae in maxime doloribus voluptates minus
-        debitis eum ullam sit obcaecati laborum quidem voluptatum sint itaque
-        officia facere, placeat esse nam quasi? Hic maiores fuga blanditiis
-        doloremque praesentium similique at maxime! Error facere deserunt cum
-        aliquam ullam maxime aspernatur. Fuga, laboriosam? Quisquam voluptates
-        atque molestiae tenetur, assumenda eum illum quaerat earum veritatis
-        sequi et accusamus similique odit, voluptatum dignissimos molestias
-        culpa sed magnam eligendi beatae. Dolore officia ex sint molestias
-        mollitia vitae, iste atque minima minus, quasi quae natus et maiores
-        quam doloribus iusto illo! Eaque quasi blanditiis explicabo.
-      </div>
->>>>>>> 7bbe315cbe811132c998c80e493c87d520cdfccc
     </>
   );
 }
